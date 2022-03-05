38351a0503ff2837ed2fee4e6b2e45c2d56dd56d
Danny <bublikplushka@yandex.ru>
2022-03-06 00:16:07 +0300

    Added Resident EVIL

---
+++ resident_evil/res.asm
@@ -0,0 +1,363 @@
+.model tiny
+.186
+.code
+org 100h
+locals @@
+
+
+;-------------------------------------------------------------------------
+; CONSTANTS BLOCK
+;-------------------------------------------------------------------------
+
+SwitchButton        =   1Ah     ; "[" on keyboard
+videoseg	        =   0b800h
+int_09_addr         =   09h * 4
+int_08_addr         =   08h * 4
+WP                  equ word ptr
+
+one_reg_name_len    = 5d
+num_of_regs         = 8d
+
+byte_array_len      = 3d
+
+fg_color            = 1Bh
+bg_color            = 0Bh  
+
+window_len          = 50h
+window_height       = 14h
+
+box_len             = 18h
+box_height          = 10h
+
+text_x_shift        = 8
+text_y_shift        = 2
+start_x             = 5
+start_y             = 5
+
+;-------------------------------------------------------------------------
+; MACRO BLOCK
+;-------------------------------------------------------------------------
+
+
+;-------------------------------------------------------------------------
+; ResidentForever says dos not to delete our evil programm from interrups table 
+;-------------------------------------------------------------------------
+ResidentForever macro
+    mov ax, 3100h
+    mov dx, offset ResidentEnd
+    add dx, 15d
+    shr dx, 04h
+    int 21h
+endm
+
+;-------------------------------------------------------------------------
+; NewLineForBox makes new line for a given register
+;-------------------------------------------------------------------------
+NewLineForBox macro reg
+    add reg, 2 * (window_len - box_len)
+endm
+
+NewLineForRegister macro reg
+    add reg, 2 * (window_len - box_len +  2 * text_x_shift)
+endm
+
+;-------------------------------------------------------------------------
+; MAIN BLOCK
+;-------------------------------------------------------------------------
+
+start:
+    xor bx, bx
+    mov es, bx
+
+    mov bx, int_09_addr
+    mov di, offset OldInt09
+    mov si, offset NewInt09_Evil
+    call ReplaceInterrupt
+
+    mov bx, int_08_addr
+    mov di, offset OldInt08
+    mov si, offset NewInt08_Evil
+    call ReplaceInterrupt
+    
+    ResidentForever
+
+    include str_lib.asm
+
+;-------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------
+; FUNCTIONS BLOCK
+;-------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------
+; ReplaceInterrupt
+; replace old interrupt with new one, but saves old one too
+; Expects:
+;           BX: addres of interrupt to replace
+;           DI: addres of buffer to save old interrupt
+;           SI: addres of new interrupt to replace old one with
+; Destroys: BX, DI, SI, ES
+;-------------------------------------------------------------------------
+
+ReplaceInterrupt proc
+    xor ax, ax
+    mov es, ax              ; moving 0 to es
+
+    ; saving old intterupt 
+    mov ax, WP es:[bx]      ; copy old-int addres to ax
+    mov [di], ax            ; move old-int addres to buffer
+
+    mov ax, WP es:[bx + 2]  ; copy old-int segment to ax
+    mov [di + 2], ax        ; move old-int segment to buffer
+
+    ; moving new int 09 to replace old
+    cli                     ; forbids to call interrupts 
+    mov ax, si
+    mov WP es:[bx], ax      ; move new-int addres to interrupts table
+    mov ax, cs
+    mov WP es:[bx + 2], ax  ; move new-int segment to interrupts table
+
+    sti                     ; allows to call interrupts 
+    
+    ret
+ReplaceInterrupt endp
+
+;-------------------------------------------------------------------------
+; NewInt09_Evil
+; custom 09 interrupt which change FrameSwitcher flag
+;
+; Destroys: All registers were pushed
+;-------------------------------------------------------------------------
+
+NewInt09_Evil proc
+    push ax bx cx dx si di es ds 
+    in al, 60h
+    cmp al, SwitchButton
+    je @@Activate
+
+    jmp @@Return
+    
+    @@Activate:
+        not cs:FrameSwitcher
+        ; mov al, cs:FrameSwitcher
+        ; add al, 'A'
+        ; mov ah, 4eh
+        ; mov bx, videoseg                                ; loads videoseg addr
+        ; mov es, bx                                      ; loads videoseg addr
+        ; mov di, 0h
+        ; mov es:[di], ax
+
+        ; TODO save screen to buffer
+
+    @@Return:
+        ; K-BRD Acepted 
+        mov ah, al
+        or al, 80h
+        out 61h, al
+        mov al, ah
+        out 61h, al
+        
+        ; EOI
+        mov al, 20h
+        out 20h, al
+        
+        pop ds es di si dx cx bx ax
+
+                    db 0EAh             ; long jump to...
+        OldInt09    dd 0                ; old 09 int. (zeros fill be filled later)
+endp
+
+;-------------------------------------------------------------------------
+; NewInt08_Evil
+; custom 08 interrupt which drows frame 
+;
+; Destroys: All registers were pushed
+;-------------------------------------------------------------------------
+
+NewInt08_Evil proc
+    push ax bx cx dx si di es ds 
+    
+    cmp cs:FrameSwitcher, 0h
+    je  @@EraseFrame
+
+    call DrawBox
+
+    @@EraseFrame:
+    ; TODO erase frame
+
+    @@Return: 
+        pop ds es di si dx cx bx ax
+
+                    db 0EAh             ; long jump to...
+        OldInt08    dd 0                ; old 08 int. (zeros fill be filled later)
+endp NewInt08_Evil
+
+;-------------------------------------------------------------------------
+; DrawBox
+; drows a frame with registers values in it
+;
+; Destroys: None
+;-------------------------------------------------------------------------
+DrawBox proc
+    push es ds di si dx cx bx ax 
+    mov ax, cs
+    mov ds, ax
+
+    mov si, offset header_symbols                   ; loads data addres to si 
+    mov ah, bg_color                                ; loads color
+    mov bx, videoseg                                ; loads videoseg addr
+    mov es, bx                                      ; loads videoseg addr
+    mov cx, box_len                                 ; loads box len
+    mov di, (start_x + start_y * window_len) * 2    ; sets correct position
+
+    call DrawLine                                   ; calls function to draw header line
+
+    xor bx, bx                                      ; sets bx to 0
+    
+    @@zaloop:
+        NewLineForBox di                            ; new line 
+        inc bx                                      ; increment cycle counter
+        mov cx, box_len                             ; loads box len to compare later
+        mov si, offset middle_symbols               ; loads pointer to next array (middle-lines array)
+
+        call DrawLine                               ; calls function to draw middle-lines
+        
+        cmp bx, box_height                          ; compare cycle counter and length of box
+    jbe @@zaloop
+
+    NewLineForBox di                                ; another new line 
+
+    mov cx, box_len                                 ; loads box len
+    mov si, offset bottom_symbols                   ; loads pointer to array with footer line
+
+    call DrawLine                                   ; calls function to draw footer line 
+
+    ; TODO draw registers - doesnt work!
+    mov cx, num_of_regs
+    mov di, (start_x + text_x_shift + (start_y + text_y_shift) * window_len) * 2
+    mov bx, offset Names_reg
+
+    @@draw_regs:
+        mov si, offset Bufer_reg
+        
+        mov ax, ds
+        mov es, ax
+
+        mov ax, di
+        mov di, si
+        mov dx, bx
+
+        pop bx
+        call itoa_16
+        
+        mov bx, dx
+        mov si, di
+        mov di, ax
+
+        mov ax, videoseg
+        mov es, ax
+        
+        mov ah, bg_color
+
+        call WriteReg
+        
+        NewLineForRegister di
+        NewLineForRegister di
+        add di, 2 * text_x_shift
+
+    loop @@draw_regs
+
+    ret
+DrawBox endp
+
+;------------------------------------------------------------------------
+; DrawLine
+;
+; Draws one a line in a frame by given characters
+;
+; Entry:    AH -- color of a line
+;           CX -- len of a line
+;           SI -- addr of 3-byte array containing line elements
+;           DI -- addres of a start of a line
+; Note:  ES -- videoseg addr (0b800h)
+; Exit:  None
+; Destr: AX, CX, DI, SI
+;------------------------------------------------------------------------
+DrawLine    proc
+    cmp cx, 2                               
+    jbe @@stopLine          ; if cx - 2 <= 0 then we shouldnt draw line
+
+    lodsb                   ; puts first symbol to a-low and increses si by 1
+    stosw                   ; puts ax (ah -- (color), al -- (symb)) to videoram and increses di by 2      
+    
+    lodsb                   ; puts second symbol to a-low and increses si by 1
+    
+    sub cx, 2                               
+    rep stosw               ; repeats while cx not zero
+                            ; puts ax (ah -- (color), al -- (symb)) to videoram and increses di by 2 
+
+    lodsb                   ; puts third symbol to a-low and increses si by 1
+    stosw                   ; puts ax (ah -- (color), al -- (symb)) to videoram and increses di by 2
+
+    @@stopLine:           
+        ret
+DrawLine    endp
+
+;------------------------------------------------------------------------
+; WriteReg
+;
+; Writes register name and its value inside box
+;
+; Entry:    AH -- color of a line
+;           BX -- addr of array containg register name str. repr.
+;           SI -- addr of array containing register value
+;           DI -- addres of a start of a line
+; Note:  ES -- videoseg addr (0b800h)
+; Exit:  None
+; Destr: AX
+;------------------------------------------------------------------------
+WriteReg    proc
+    push cx
+
+    mov cx, si
+    mov si, bx
+    jmp @@start_name_cycle
+
+    @@write_name:
+        stosw
+        @@start_name_cycle:
+        lodsb
+        cmp al, 0
+    jne @@write_name
+    
+    mov bx, si  ; for next iteration
+
+    mov si, cx
+    jmp @@start_value_cycle
+
+    @@write_value:
+        stosw
+    @@start_value_cycle:
+        lodsb
+        cmp al, 0
+    jne @@write_value
+
+    pop cx
+    ret
+WriteReg    endp
+
+;-------------------------------------------------------------------------
+; DATA BLOCK
+;-------------------------------------------------------------------------
+.data
+FrameSwitcher   db   0h
+
+header_symbols  db  "ÉÍ»"
+middle_symbols  db  "º º"
+bottom_symbols  db  "ÈÍ¼"
+
+Names_reg       db  "AX: ", 0, "BX: ", 0, "CX: ", 0, "DX: ", 0, "SI: ", 0, "DI: ", 0, "DS: ", 0, "ES: ", 0
+Bufer_reg       db  4 dup ('0'), 0
+
+ResidentEnd:
+end start
---
+++ resident_evil/str_lib.asm
@@ -0,0 +1,492 @@
+locals @@
+.186
+
+;------------------------------------------------------------------------
+; StrLen
+;
+; Counts the length of the given string
+;
+; Entry:    SI -- addr of 0-terminated string
+;
+; Exit:     CX -- length of the string without 0-symbol
+;
+; Destr:    CX
+;------------------------------------------------------------------------
+StrLen    proc
+
+    push ax si bp
+    mov bp, sp
+    
+    xor cx, cx
+
+    lodsb
+    cmp al, 0
+    je @@return     ; if string contains only 0-terminator char
+
+    @@zaloop:
+        inc cx
+        lodsb
+        cmp al, 0
+    jne @@zaloop
+
+    @@return:
+
+        pop bp si ax
+        ret
+StrLen    endp
+
+;------------------------------------------------------------------------
+
+;------------------------------------------------------------------------
+; StrChr
+;
+; Findes the position of given char in given string
+;
+; Entry:    SI -- addr of 0-terminated string
+;           BL -- char to find
+;
+; Exit:     CX -- Position of given char in given string or -1
+;
+; Destr:    CX
+;------------------------------------------------------------------------
+
+StrChr    proc
+    push ax bx si bp
+    mov bp, sp
+
+    mov bl, [bp + 6]
+    mov si, [bp + 2]
+
+    xor cx, cx
+
+    @@zaloop:
+        lodsb
+
+        cmp al, bl
+        je  @@return
+
+        inc cx
+        cmp al, 0
+    jne @@zaloop
+
+    ; if char isnt found and loop is over:
+    mov cx, -1
+    jmp @@return
+
+    ; if char is found
+    @@return:
+        pop bp si ax
+        ret
+
+StrChr    endp
+
+;------------------------------------------------------------------------
+
+;------------------------------------------------------------------------
+; StrNCpy
+;
+; copies the first n chars of the first string to aother place 
+;
+; Entry:    SI -- addr of 0-terminated string
+;           DI -- new addres to copy string
+;           CX -- num chars to copy
+;
+; Exit:     None     
+;
+; Destr:    None
+;------------------------------------------------------------------------
+
+StrNCpy    proc
+    push bx cx di si bp
+    mov bp, sp
+
+    mov cx, [bp + 8]
+    mov di, [bp + 6]
+    mov si, [bp + 4]
+
+    @@zaloop:
+        movsb                   ; [di] -> [si], inc si, inc di 
+        mov bl, ds:[di]
+        cmp bl, 0               ; checks 0-terminating symbol
+        je @@string_was_ended 
+    loop @@zaloop
+
+    @@string_was_ended:
+        mov bl, 0
+        mov ds:[di], bl
+
+        pop bp si di cx bx
+        ret
+
+StrNCpy    endp
+
+;------------------------------------------------------------------------
+
+;------------------------------------------------------------------------
+; StrNCmp
+;
+; compares the first n chars of the given strins
+;
+; Entry:    SI -- addr of first 0-terminated string
+;           DI -- addr of second 0-terminated string
+;           CX -- num chars to compare
+;
+; Exit:     in ax: 0 if str1 == str2, less 0 if str1 < str2 or more 0 if str1 > str2     
+;
+; Destr:    AX
+;------------------------------------------------------------------------
+
+StrNCmp    proc
+    push bx cx di si bp
+    mov bp, sp
+
+    mov cx, [bp + 6]
+    mov di, [bp + 4]
+    mov si, [bp + 2]
+
+    @@zaloop:
+        mov al, [si]         
+        mov bl, [di]
+
+
+        sub ax, bx
+        inc si
+        inc di
+
+        cmp ax, 0
+        jne @@not_equals_chars
+
+        cmp bx, 0
+        je @@strings_are_ended
+    loop @@zaloop
+    
+    ; if loop is ended, but all chars are the same
+    @@strings_are_ended:
+        mov ax, 0
+    
+    @@not_equals_chars:
+        pop bp si di cx bx
+        ret 
+
+StrNCmp    endp
+
+;------------------------------------------------------------------------
+
+
+;------------------------------------------------------------------------
+; itoa_16
+;
+; converts 16-based integer number to str
+;
+; Entry:    BX -- number to be converted
+;           DI -- addr to put string
+;
+; Exit:     None
+;
+; Destr:    None
+;------------------------------------------------------------------------
+
+itoa_16      proc
+    push ax bx cx di si bp
+
+    ; skip leading zeros
+    mov cx, 4d
+    ; jmp @@check_leading_zero
+    
+    ; @@skip_leading_zeros:
+    ;     shl bx, 4d
+    ;     dec cx
+    ; @@check_leading_zero:
+    ;     mov ax, bx
+    ;     shr ax, 12 
+    ;     cmp ax, 0
+    ; je @@skip_leading_zeros
+
+
+    @@convert_num_to_str:
+        mov si, offset str_for_16
+        mov ax, bx
+        shr ax, 12d
+        add si, ax
+        mov ax, [si]
+        stosb
+        shl bx, 4d
+    loop @@convert_num_to_str
+
+    xor ax, ax
+    stosb
+
+    pop bp si di cx bx ax
+    ret
+itoa_16    endp
+
+;------------------------------------------------------------------------
+; itoa_10
+;
+; converts 10-based integer number to str
+;
+; Entry:    BX -- number to be converted
+;           DI -- addr to put string
+;
+; Exit:     None     
+;
+; Destr:    None
+;------------------------------------------------------------------------
+
+itoa_10    proc
+    push ax bx cx dx di si bp
+    mov bp, sp
+    
+    mov di, [bp + 4]
+    mov bx, [bp + 10]
+
+    ; count len of the number
+    mov ax, bx
+    mov dl, 10d
+
+    @@count_len_cycle:
+        inc cx
+        div dl           ; ax <- ax / 10, dx
+        mov ah, 0
+
+        cmp al, 0
+    jne @@count_len_cycle
+
+    
+    mov si, di
+    add si, cx
+    mov ah, 0
+    mov [si], ah
+
+    mov ax, bx
+
+    @@translate_int_to_str:
+        dec cx
+
+        div dl          ; al <- ax / 10, ah <- ah / 10
+        
+        mov si, di
+        add si, cx
+        add ah, '0'
+        mov [si], ah
+        mov ah, 0
+
+        cmp cx, 0
+    ja @@translate_int_to_str
+
+    pop bp si di dx cx bx ax
+    ret
+itoa_10    endp
+
+;------------------------------------------------------------------------
+; itoa_8
+;
+; converts 8-based integer number to str
+;
+; Entry:    BX -- number to be converted
+;           DI -- addr to put string
+;
+; Exit:     None
+;
+; Destr:    None
+;------------------------------------------------------------------------
+itoa_8      proc
+    push ax bx cx di bp
+    mov bp, sp
+    
+    mov di, [bp + 2]
+    mov bx, [bp + 6]
+
+    mov cx, 6       ; num of digits
+
+    ; skip leading zeros
+    mov ax, bx
+    shr ax, 15d
+    cmp ax, 0
+    shl bx, 1
+    jne @@no_leading_zeros 
+
+    mov cx, 5       ; if there's no leading 1, than we have no more than 5 digits
+    jmp @@check_leading_zero
+    
+    @@skip_leading_zeros:
+        shl bx, 3d
+        dec cx
+    @@check_leading_zero:
+        mov ax, bx
+        shr ax, 13d
+        cmp ax, 0 
+    je @@skip_leading_zeros
+    
+    @@convert_num_to_str:
+        mov ax, bx
+        shr ax, 13d
+        shl bx, 3
+    @@no_leading_zeros:
+        add ax, '0'
+        stosb
+    loop @@convert_num_to_str
+
+    xor ax, ax
+    stosb
+
+    pop bp di cx bx ax
+    ret
+itoa_8    endp
+
+;------------------------------------------------------------------------
+; itoa_2
+;
+; converts 2-based integer number to str
+;
+; Entry:    BX -- number to be converted
+;           DI -- addr to put string
+;
+; Exit:     None
+;
+; Destr:    None
+;------------------------------------------------------------------------
+
+itoa_2      proc
+    push ax bx cx di bp
+    mov bp, sp
+    
+    mov di, [bp + 2]
+    mov bx, [bp + 6]
+
+    ; skip leading zeros
+    mov cx, 16d
+    jmp @@check_leading_zero
+    
+    @@skip_leading_zeros:
+        shl bx, 1
+        dec cx
+    @@check_leading_zero:
+        mov ax, bx
+        shr ax, 15 
+        cmp ax, 0
+    je @@skip_leading_zeros
+
+    @@convert_num_to_str:
+        mov ax, bx
+        shr ax, 15
+        add ax, '0'
+        stosb
+        shl bx, 1
+    loop @@convert_num_to_str
+
+    xor ax, ax
+    stosb
+
+    pop bp di cx bx ax
+    ret
+itoa_2    endp
+
+
+;------------------------------------------------------------------------
+; atoi_10
+;
+; converts str to 10-based integer
+;
+; Entry:    SI -- addr of string
+;
+; Exit:     BX -- result of convertation
+;
+; Destr:    BX
+;------------------------------------------------------------------------
+atoi_10  proc
+    push ax cx si bp
+    mov bp, sp
+    
+    mov di, [bp + 2]
+
+    xor bx, bx
+    mov cx, 10d
+    jmp @@start_cycle
+    
+    @@convert_str_to_num:
+        ; mul bx by 10...
+        mul cx
+
+        sub bl, '0'
+        add al, bl
+    @@start_cycle:
+        mov bl, [si]
+        inc si
+
+        cmp bl, 0
+    jne @@convert_str_to_num
+
+    mov bx, ax
+    pop bp si cx ax
+
+    ret
+atoi_10  endp
+
+;------------------------------------------------------------------------
+; atoi_8
+;
+; converts str to 8-based integer
+;
+; Entry:    SI -- addr of string
+;
+; Exit:     BX -- result of convertation
+;
+; Destr:    BX
+;------------------------------------------------------------------------
+atoi_8  proc
+    push ax si bp
+    mov bp, sp
+    
+    mov si, [bp + 2]
+
+    xor bx, bx
+    jmp @@start_cycle
+    
+    @@convert_str_to_num:
+        shl bx, 3           ; this is the only thing different from atoi_2, maybe add macros (cringe)
+        sub al, '0'
+        add bl, al
+    @@start_cycle:
+        lodsb
+        cmp al, 0
+    jne @@convert_str_to_num
+
+    pop bp si ax
+    ret
+atoi_8  endp
+
+;------------------------------------------------------------------------
+; atoi_2
+;
+; converts str to 2-based integer
+;
+; Entry:    SI -- addr of string
+;
+; Exit:     BX -- result of convertation
+;
+; Destr:    BX
+;------------------------------------------------------------------------
+atoi_2  proc
+    push ax si bp
+    mov bp, sp
+
+    mov si, [bp + 2]
+
+    xor bx, bx
+    jmp @@start_cycle
+    
+    @@convert_str_to_num:
+        shl bx, 1
+        sub al, '0'
+        add bl, al
+    @@start_cycle:
+        lodsb
+        cmp al, 0
+    jne @@convert_str_to_num
+
+    pop bp si ax
+    ret
+atoi_2  endp
+
+str_for_16  db "0123456789ABCDEF"
